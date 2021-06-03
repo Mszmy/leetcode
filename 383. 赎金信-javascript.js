332d46adfc9af21e19dfb0764e98e575b5cd68eb
wanjun_Y <46343874+Mszmy@users.noreply.github.com>
2021-06-03 18:42:09 +0800

    Create 383. 赎金信-javascript.js

---
+++ 383. 赎金信-javascript.js
@@ -0,0 +1,9 @@
+var canConstruct = function(ransomNote, magazine) {
+   for(var i=0; i<ransomNote.length;i++){
+       var index = magazine.indexOf(ransomNote[i])
+       if(index ==-1)return false
+       magazine = magazine.substring(0,index) + magazine.substring(index+1,magazine.length)
+   }
+   return true;
+    
+};
